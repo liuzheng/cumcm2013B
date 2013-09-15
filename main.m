ecbc261a2883e108f2536ee3e39db1de81a78af1
liuzheng712 <liuzheng712@gmail.com>
2013-09-15 16:05:53 +0800

    1 is ok

--- main.m
+++ main.m
@@ -5,18 +5,25 @@ img = [];
 for i = 1 : length(file)
     img(:,:,i) = imread([path '/' file(i).name]);
 end
-for j = 1 :length(file)
-    s = 0;ss = 0;
+for j = 1 : length(file)
     for i = 1 : length(file)
         if j==i
+            S(j,i) = Inf;
             continue
         end
-        ss = s;
-        s = sum((img(:,2,j)-img(:,end-1,i)).^2);
-        if s>ss
-            ss = s;
-            left(j) = i;
-        end
+        S(j,i) = sum((img(:,end,j)-img(:,1,i)).^2);
     end
 end
-left
+for i = 1 : length(file)
+    left(find(S(:,i)==min(S(:,i)))) = i;
+    if img(:,end,i)==255
+        START = i;
+    end
+end
+IMG = img(:,:,START);
+
+for i = 1:length(file)-1
+    IMG = [img(:,:,find(left==START)) IMG ];
+    START = find(left==START);
+end
+imshow(IMG)
